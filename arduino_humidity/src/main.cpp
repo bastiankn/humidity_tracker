0d4a9e15a0b9974645627f3eb375bfb693cd25f5
BKBastian <bastian.katzmann@web.de>
2024-06-09 10:30:15 +0200

    added working arduino code (except for some error

--- arduino_humidity/src/main.cpp
+++ arduino_humidity/src/main.cpp
@@ -1,18 +1,42 @@
 #include <Arduino.h>
+#include "dht11.h"
 
-// put function declarations here:
-int myFunction(int, int);
+#define DHT11PIN 4
 
-void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+dht11 DHT11;
+
+void setup()
+{
+  Serial.begin(9600);
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
-}
+void loop()
+{
+  Serial.println();
 
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
+  int chk = DHT11.read(DHT11PIN);
+
+  switch (chk)
+  {
+  case DHTLIB_OK:
+    Serial.println("DHT11 OK");
+    break;
+  case DHTLIB_ERROR_CHECKSUM:
+    Serial.println("DHT11 Checksum error");
+    break;
+  case DHTLIB_ERROR_TIMEOUT:
+    Serial.println("DHT11 Time out error");
+    break;
+  default:
+    Serial.println("DHT11 Unknown error");
+    break;
+  }
+
+  Serial.print("Humidity (%): ");
+  Serial.println(DHT11.humidity);
+
+  Serial.print("Temperature (C): ");
+  Serial.println(DHT11.temperature);
+
+  delay(2000);
 }
