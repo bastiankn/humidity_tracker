9a97ee05db7cbb5023f8c2c8a5d6a5f4e8d94530
Bastian Katzmann <bastian.katzmann@web.de>
2024-06-23 10:23:20 +0200

    changed database path

--- backend/main.py
+++ backend/main.py
@@ -6,9 +6,6 @@ import time
 from datetime import datetime
 import init_db
 
-# Delete older than 24hrs records
-init_db.delete_old_data("backend/sensor.db")
-
 # Setup Streamlit
 st.title("DHT11 Sensor Data")
 
@@ -51,7 +48,7 @@ def main():
     Main function to continuously read data from the serial port,
     update Streamlit display, and insert data into the database.
     """
-    init_db.delete_old_data("backend/sensor.db")
+    init_db.delete_old_data("sensor.db")
     serial_port = '/dev/ttyACM0'  # Adjust to your actual serial port
     ser = serial.Serial(serial_port, 9600, timeout=1)
     insert_data = True
